d9412e8a466cd3fc463f2590126030e21ca033bd
jeremy <collyerdesign@gmail.com>
2016-07-07 07:00:35 -0400

    hit jobcoin api with superagent

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import LoginForm from './login-form'
+import request from 'superagent'
 
 export default class Login extends Component {
   handleSubmit(stateData) {
-    debugger;
+    request
+      .get('http://jobcoin.projecticeland.net/fish-sticks/api/addresses/'+stateData.address)
+      // .send({ name: 'Manny', species: 'cat' })
+      // .set('X-API-Key', 'foobar')
+      // .set('Accept', 'application/json')
+      .end(function(err, res){
+        // Calling the end function will send the request
+        debugger;
+      });
   }
   render() {
     return (
